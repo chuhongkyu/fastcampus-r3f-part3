22e07aa36c8476a748979efca7d598c2a35b4592
Mr.chu <chuhongkyu@gmail.com>
2023-08-30 16:10:28 +0900

    Add class_09

--- class_01/src/App.js
+++ class_01/src/App.js
@@ -1,9 +1,18 @@
 import { Canvas } from "@react-three/fiber";
+import { Box } from "./Box";
 
 function App() {
   return (
     <>
-    
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color='orange' />
+        </mesh>
+        {/* <Box/> */}
+      </Canvas>
     </>
   );
 }
---
+++ class_09/src/components/Ground.jsx
@@ -0,0 +1,34 @@
+import { usePlane } from "@react-three/cannon";
+import { useRef } from "react";
+import { ColliderBox } from "./ColliderBox";
+
+export function Ground() {
+  const [ref] = usePlane(
+    () => ({ 
+      type: 'Static', 
+      rotation: [-Math.PI / 2, 0, 0] }
+    ), 
+    useRef(null)
+  );
+
+  return (
+    <>
+      {/* <mesh ref={ref} receiveShadow>
+        <planeGeometry args={[24, 24]} />
+        <meshBasicMaterial
+          opacity={1}
+          transparent={true}
+          color={0xFFFF00}
+        />
+      </mesh> */}
+      {/* 앞 */}
+      <ColliderBox position={[0, 0.5, 5]} scale={[7, 1, 0.3]}/>
+      {/* 뒤 */}
+      <ColliderBox position={[0, 0.5, -5]} scale={[7, 1, 0.3]}/>
+      {/* 좌 */}
+      <ColliderBox position={[3.5, 0.5, 0]} scale={[0.3, 1, 10]}/>
+      {/* 우 */}
+      <ColliderBox position={[-3.5, 0.5, 0]} scale={[0.3, 1, 10]}/>
+    </>
+  );
+}
