2b92c6f1713fea2c51b2474608377705b7eb2698
Mr.chu <chuhongkyu@gmail.com>
2023-09-25 23:55:52 +0900

    Add Loading

--- class_last/src/Scene.jsx
+++ class_last/src/Scene.jsx
@@ -1,17 +1,14 @@
-import {
-  Environment,
-  OrbitControls,
-  Stats,
-} from "@react-three/drei";
+import { OrbitControls, Stats } from "@react-three/drei";
 import { Suspense } from "react";
 import { Ground } from "./components/Ground";
 import { Car } from "./Car";
 import { Lights } from "./components/Lights";
+import { Loading } from "./components/Loading";
 
 export function Scene() {
 
   return (
-    <Suspense fallback={null}>
+    <Suspense fallback={<Loading/>}>
       {/* <Stats/> */}
       <Lights color="#fad6a1"/>
       <OrbitControls enableZoom={false} enableRotate={false} target={[0,0.5,0]}/>
---
+++ class_last/src/components/Loading.jsx
@@ -0,0 +1,5 @@
+export const Loading = () =>{
+    return(
+        <span className="loader"></span>
+    )
+}
