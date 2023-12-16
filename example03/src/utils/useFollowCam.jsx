7919cbcbe0c763af0c243a67ab818af16516156f
Mr.chu <chuhongkyu@gmail.com>
2023-12-16 13:29:59 +0900

    fixed FollowCam

--- example03/src/utils/useFollowCam.jsx
+++ example03/src/utils/useFollowCam.jsx
@@ -7,22 +7,12 @@ const useFollowCam = () => {
   const { scene, camera } = useThree()
   // Object3D 인스턴스를 memoize하여 pivot 포인트를 만듭니다.
   const pivot = useMemo(() => new Object3D(), [])
-  
-  // 초기 위치가 (0, 0, 0)인 팔로잉 카메라를 만듭니다.
-  const followCam = useMemo(() => {
-    const o = new Object3D()
-    o.position.set(0, 0, 0)
-    return o
-  }, [])
 
   const makeCamera = ()=>{
     camera.position.set(1,2,3.5)
     camera.rotation.x = -0.5
-    // 메인 카메라를 팔로잉 카메라에 추가합니다.
-    followCam.add(camera)
-    // 팔로잉 카메라를 pivot 포인트에 추가합니다.
-    pivot.add(followCam)
-    // pivot 포인트를 scene에 추가합니다.
+
+    pivot.add(camera)
     scene.add(pivot)
   }
 
@@ -30,7 +20,6 @@ const useFollowCam = () => {
     makeCamera()
   }, [])
 
-  // 외부에서 pivot 포인트를 사용할 수 있도록 반환합니다.
   return { pivot }
 }
 
--- example06/src/utils/useFollowCam.jsx
+++ example06/src/utils/useFollowCam.jsx
@@ -8,17 +8,12 @@ export default function useFollowCam() {
   const { scene, camera } = useThree()
   const isStart = useRecoilValue(onStartScene)
   const pivot = useMemo(() => new Object3D(), [])
-  const followCam = useMemo(() => {
-    const o = new Object3D()
-    o.position.set(0, 0, 0)
-    return o
-  }, [])
 
   const makeCamera = ()=>{
     camera.position.set(1,2,3.5)
     camera.rotation.x = -0.5
-    followCam.add(camera)
-    pivot.add(followCam)
+
+    pivot.add(camera)
     scene.add(pivot)
   }
 
