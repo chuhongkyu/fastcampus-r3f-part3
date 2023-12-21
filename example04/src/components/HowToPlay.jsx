073eaf261be28dfe62c081d1974b2e8146851dab
Mr.chu <chuhongkyu@gmail.com>
2023-12-21 21:14:29 +0900

    Add example04

--- example04/src/components/HowToPlay.jsx
+++ example04/src/components/HowToPlay.jsx
@@ -1,45 +1,44 @@
-import { Text3D } from "@react-three/drei";
+import { Text, Text3D } from "@react-three/drei";
 
 const HowToPlay = () => {
+
     const fontUrl = '/assets/fonts/Pretendard.json'
 
     const fontStyle = {
-        letterSpacing: 0.01,
-        size: 0.15, 
         font: fontUrl,
+        size: 0.15,
+        letterSpacing: 0.01,
         height: 0.02,
-        lineHeight: 1,
-        fontSize: 1,
+        lineHeight: 1, 
+        fontSize: 1
     }
 
     return(
-        <group  
-            position={[0.5, 0, 1.2]} 
-            rotation={[-Math.PI/2,0, 0]}>
-            <Text3D 
-                {...fontStyle}>
+        <group position={[0.3,0,1]} rotation={[-Math.PI/2, 0, 0]}>
+            <Text3D
+                {...fontStyle}
+                >
                 How to Play
-                <meshBasicMaterial color="gray"/>
+                <meshNormalMaterial/>
             </Text3D>
             <group position={[0.3, -0.5, 0]}>
                 <Text3D
-                    position={[0.2,0.1,0]}
-                    {...fontStyle}>
+                    position={[0.2, 0.1, 0]}
+                    {...fontStyle}
+                    >
                     ↑
-                    <meshBasicMaterial color="gray"/>
+                    <meshNormalMaterial/>
                 </Text3D>
-                <Text3D 
+                <Text3D
                     position={[0,-0.1,0]}
-                    {...fontStyle}>
-                        ←↓→
-                        <meshBasicMaterial color="gray"/>
+                    {...fontStyle}
+                    >
+                    ←↓→
+                    <meshNormalMaterial/>
                 </Text3D>
             </group>
-            
         </group>
     )
 }
 
 export default HowToPlay;
-
-
